0d9761ec19926be01e7e628b96f8eabcc59231e7
saipotnuri22 <potnurisai286@gmail.com>
2025-03-23 19:02:53 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 align="center">Welcome to the Food Delivery</h1>
+      <img src="./maxresdefault.jpg" alt="" style={{ display: 'block', margin: '10px auto', width: '1000px', height: 'auto', padding: '20px' }} />
+    </div>
+  )
+}
+
+export default Home
